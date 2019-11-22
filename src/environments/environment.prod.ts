b4c332e4d4eb3e09d06b6ad6d1b946fabac7a096
Alfred Cron <alfred.cron@gmail.com>
2019-11-22 13:53:39 +0100

    paramétrage url correctif

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: 'AIzaSyCUi7oUaplGbfgnU9fQFx_5QkNJMHP5kSg'},
-    apiUrl : 'https://my-json-server.typicode.com/alfredcron/pizzaparty'
+    apiUrl : 'https://my-json-server.typicode.com/alfredcron/pizzaparty/'
 }
